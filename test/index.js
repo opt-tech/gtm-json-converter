f64444da09242883ed85187bf06c5db32137999b
matzkoh <matzkoh@gmail.com>
2019-05-24 20:12:36 +0900

    fix: replace illegal characters at filename

--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ function getPath(name) {
 
 const { outputFileName, data } = convert(getPath('test.json'))
 
-assert(outputFileName === getPath('[example.com] 1970-01-01 00:00:00.csv'))
+assert(outputFileName === getPath('[example.com] 1970-01-01 010203.csv'))
 
 const expected = readFileSync(getPath('test.csv'), 'utf-8')
   .slice(1)
